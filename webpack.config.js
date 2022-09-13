9e8a4d08f1ab01b4037ef5e5732b1f08a351e29e
PanasiukNazar <nazar_panasiuk.ua@gmail.com>
2022-09-13 12:01:29 +0300

    add dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,70 +7,67 @@ const CssMinimizerExtractPlugin = require('css-minimizer-webpack-plugin');
 const TraserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = (env) => {
-   const isProduction = env.production;
-   const isDevelopment = !isProduction;
+  const isProduction = env.production;
+  const isDevelopment = !isProduction;
 
-   const optimizeJs = isDevelopment ? 'main.js' : 'main.[contenthash].js';
-   const optimizeCss = isDevelopment ? 'main.css' : 'main.[contenthash].css';
+  const optimizeJs = isDevelopment ? 'main.js' : 'main.[contenthash].js';
+  const optimizeCss = isDevelopment ? 'main.css' : 'main.[contenthash].css';
 
-   return {
-      mode: isProduction ? 'production' : 'development',
+  return {
+    mode: isProduction ? 'production' : 'development',
 
-      entry: './src/app/main.js',
+    entry: './src/app/main.js',
 
-      output: {
-         filename: optimizeJs,
-         path: path.resolve(__dirname, 'dist'),
-      },
+    output: {
+      filename: optimizeJs,
+      path: path.resolve(__dirname, 'dist'),
+    },
 
-      devtool: 'source-map',
+    devtool: 'source-map',
 
-      optimization: {
-         minimizer: [
-            new CssMinimizerExtractPlugin(),
-            new TraserWebpackPlugin(),
-         ],
-      },
+    optimization: {
+      minimizer: [new CssMinimizerExtractPlugin(), new TraserWebpackPlugin()],
+    },
 
-      plugins: [
-         new HtmlWebpackPlugin({
-            template: './src/index.html',
-            minify: {
-               removeAttributeQuotes: isProduction,
-               collapseWhitespace: isProduction,
-               removeComments: isProduction,
-            },
-         }),
-         new CleanWebpackPlugin(),
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: {
+          removeAttributeQuotes: isProduction,
+          collapseWhitespace: isProduction,
+          removeComments: isProduction,
+        },
+      }),
+      new CleanWebpackPlugin(),
 
-         new MiniCssExtractPlugin({
-            filename: 'main.scss',
-            filename: optimizeCss,
-         }),
+      new MiniCssExtractPlugin({
+        filename: 'main.scss',
+        filename: optimizeCss,
+      }),
 
-         new CopyWrbpackPlugin({
-            patterns: [
-               {
-                  from: path.resolve(__dirname, './src/assets'),
-                  to: path.resolve(__dirname, 'dist'),
-                  noErrorOnMissing: true,
-               },
-            ],
-         }),
+      new CopyWrbpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, './src/assets'),
+            to: path.resolve(__dirname, 'dist'),
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+
+        {
+          test: /\.s[ca]ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
       ],
-
-      module: {
-         rules: [
-            {
-               test: /\.css$/,
-               use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-
-            {
-               test: /\.s[ca]ss$/,
-               use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-         ],
-      },
-   };
+    },
+  };
 };
